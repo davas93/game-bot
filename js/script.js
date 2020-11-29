8dc04e37c74bf607ead3787e444875b74f200443
davas93 <vasin.da93@gmail.com>
2020-11-29 18:04:29 +0300

    complete

--- js/script.js
+++ js/script.js
@@ -3,33 +3,46 @@
 //Объявляем переменную и присвоим ей значение рандомного числа от 1 до 100 с округлением до ближайнего меньшего
 let number = Math.floor(Math.random() * (100 - 1) + 1);
 
-console.log(number); //Выведем сразу его в консоль, чтобы не искать бесконечно
-
 //Запишем функцию на проверку на число в переменную
 let isNumber = function (num) {
   return !isNaN(parseFloat(num)) && isFinite(num);
 };
 
-function gameBot() {
-  let enterNum = prompt("Угадай число от 1 до 100");
+function gameBot(attempts, number) {
+  console.log(number); //Выведем сразу его в консоль, чтобы не искать бесконечно
+
+  let enterNum = prompt("Угадай число от 1 до 10");
   if (enterNum === null) {
     alert("Игра окончена");
     return enterNum;
   }
+  if (attempts === 0) {
+    if (confirm("Попытки закончились! Хотите сыграть еще?")) {
+      number = Math.floor(Math.random() * (100 - 1) + 1);
+      return gameBot(9, number);
+    }
+    alert("Игра окончена");
+    return null;
+  }
   if (isNumber(enterNum)) {
     enterNum = +enterNum;
     if (enterNum > number) {
-      alert("Загаданное число меньше");
+      alert("Загаданное число меньше, осталось попыток: " + attempts);
     } else if (enterNum < number) {
-      alert("Загаданное число больше");
-    } else if (enterNum === number) {
-      alert("Поздравляю, Вы угадали!!!");
-      return enterNum;
+      alert("Загаданное число больше, осталось попыток: " + attempts);
+    }
+    if (enterNum === number) {
+      if (confirm("Поздравляю, Вы угадали!!! Хотите сыграть еще?")) {
+        number = Math.floor(Math.random() * (100 - 1) + 1);
+        return gameBot(9, number);
+      }
+      alert("Игра окончена");
+      return null;
     }
   } else {
     alert("Введи число!");
   }
-  return gameBot();
+  return gameBot(--attempts, number);
 }
 
-gameBot();
+gameBot(9, number);
